73d3d034306264ea002209ea34814f683d9be2be
RedBullSlurpie <redbullslurpie@gmail.com>
2019-06-27 22:19:49 -0400

    Updated code
    Updated code and gave credit where needed for other code im using
    Updated update.json

--- src/main/java/com/rbs/slurpiesdongles/init/ModTools.java
+++ src/main/java/com/rbs/slurpiesdongles/init/ModTools.java
@@ -7,6 +7,7 @@ import com.rbs.slurpiesdongles.items.tools.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemTier;
 import net.minecraft.item.Rarity;
+import net.minecraft.item.crafting.Ingredient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,23 +27,35 @@ public class ModTools {
     public static Item WITHERED_AXE = new CustomAxe("withered_axe", ToolMaterials.WITHERED, 8, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles).rarity(Rarity.EPIC), HarvestLevelHelper.TOPAZ);
 
     //Battleaxes
-    public static Item AMAZONITE_BATTLEAXE = new CustomSword("amazonite_battleaxe", ToolMaterials.AMAZONITE_BATTLEAXE, 5, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item AMETHYST_BATTLEAXE = new CustomSword("amethyst_battleaxe", ToolMaterials.AMETHYST_BATTLEAXE, 5, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item DIAMOND_BATTLEAXE = new CustomSword("diamond_battleaxe", ItemTier.DIAMOND, 4, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item EMERALD_BATTLEAXE = new CustomSword("emerald_battleaxe", ToolMaterials.EMERALD_BATTLEAXE, 5, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item GOLD_BATTLEAXE = new CustomSword("gold_battleaxe", ItemTier.GOLD, 4, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item IRON_BATTLEAXE = new CustomSword("iron_battleaxe", ItemTier.IRON, 4, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item PERIDOT_BATTLEAXE = new CustomSword("peridot_battleaxe", ToolMaterials.PERIDOT_BATTLEAXE, 3, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item RUBY_BATTLEAXE = new CustomSword("ruby_battleaxe", ToolMaterials.RUBY_BATTLEAXE, 4, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item SAPPHIRE_BATTLEAXE = new CustomSword("sapphire_battleaxe", ToolMaterials.SAPPHIRE_BATTLEAXE, 4, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item STONE_BATTLEAXE = new CustomSword("stone_battleaxe", ItemTier.STONE, 4, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
-    public static Item TOPAZ_BATTLEAXE = new CustomSword("topaz_battleaxe", ToolMaterials.TOPAZ_BATTLEAXE, 7, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles).rarity(Rarity.RARE));
-    public static Item WITHERED_BATTLEAXE = new WitheredSword("withered_battleaxe", ToolMaterials.WITHERED_BATTLEAXE, 8, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles).rarity(Rarity.EPIC));
-    public static Item WOODEN_BATTLEAXE = new CustomSword("wooden_battleaxe", ItemTier.WOOD, 4, -3.0F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item AMAZONITE_BATTLEAXE = new CustomSword("amazonite_battleaxe", ToolMaterials.AMAZONITE_BATTLEAXE, 5, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item AMETHYST_BATTLEAXE = new CustomSword("amethyst_battleaxe", ToolMaterials.AMETHYST_BATTLEAXE, 5, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item DIAMOND_BATTLEAXE = new CustomSword("diamond_battleaxe", ItemTier.DIAMOND, 4, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item EMERALD_BATTLEAXE = new CustomSword("emerald_battleaxe", ToolMaterials.EMERALD_BATTLEAXE, 5, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item GOLD_BATTLEAXE = new CustomSword("gold_battleaxe", ItemTier.GOLD, 4, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item IRON_BATTLEAXE = new CustomSword("iron_battleaxe", ItemTier.IRON, 4, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item PERIDOT_BATTLEAXE = new CustomSword("peridot_battleaxe", ToolMaterials.PERIDOT_BATTLEAXE, 3, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item RUBY_BATTLEAXE = new CustomSword("ruby_battleaxe", ToolMaterials.RUBY_BATTLEAXE, 4, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item SAPPHIRE_BATTLEAXE = new CustomSword("sapphire_battleaxe", ToolMaterials.SAPPHIRE_BATTLEAXE, 4, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item STONE_BATTLEAXE = new CustomSword("stone_battleaxe", ItemTier.STONE, 4, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item TOPAZ_BATTLEAXE = new CustomSword("topaz_battleaxe", ToolMaterials.TOPAZ_BATTLEAXE, 7, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles).rarity(Rarity.RARE));
+    public static Item WITHERED_BATTLEAXE = new WitheredSword("withered_battleaxe", ToolMaterials.WITHERED_BATTLEAXE, 8, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles).rarity(Rarity.EPIC));
+    public static Item WOODEN_BATTLEAXE = new CustomSword("wooden_battleaxe", ItemTier.WOOD, 4, -3.1F, new Item.Properties().group(Reference.tabSlurpiesDongles));
 
     //Hammers
-    public static Item TOPAZ_HAMMER = new CustomHammer("topaz_hammer", ToolMaterials.TOPAZ, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles), HarvestLevelHelper.TOPAZ);
-    public static Item WITHERED_HANMMER = new CustomHammer("withered_hammer", ToolMaterials.WITHERED, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles), HarvestLevelHelper.TOPAZ);
+    public static Item AMAZONITE_HAMMER = new Hammer("amazonite_hammer", ToolMaterials.AMAZONITE, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item AMETHYST_HAMMER = new Hammer("amethyst_hammer", ToolMaterials.AMETHYST, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item DIAMOND_HAMMER = new Hammer("diamond_hammer", ItemTier.DIAMOND, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item EMERALD_HAMMER = new Hammer("emerald_hammer", ToolMaterials.EMERALD, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item GOLD_HAMMER = new Hammer("gold_hammer", ItemTier.GOLD, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item IRON_HAMMER = new Hammer("iron_hammer", ItemTier.IRON, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item PERIDOT_HAMMER = new Hammer("peridot_hammer", ToolMaterials.PERIDOT, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item RUBY_HAMMER = new Hammer("ruby_hammer", ToolMaterials.RUBY, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item SAPPHIRE_HAMMER = new Hammer("sapphire_hammer", ToolMaterials.SAPPHIRE, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item STONE_HAMMER = new Hammer("stone_hammer", ItemTier.STONE, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item TOPAZ_HAMMER = new Hammer("topaz_hammer", ToolMaterials.TOPAZ, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item WITHERED_HANMMER = new Hammer("withered_hammer", ToolMaterials.WITHERED, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item WOODEN_HAMMER = new Hammer("wooden_hammer", ItemTier.WOOD, 1, -2.8F, new Item.Properties().group(Reference.tabSlurpiesDongles));
+
     //Hoes
     public static Item AMAZONITE_HOE = new CustomHoe("amazonite_hoe", ToolMaterials.AMAZONITE, 1, new Item.Properties().group(Reference.tabSlurpiesDongles));
     public static Item AMETHYST_HOE = new CustomHoe("amethyst_hoe", ToolMaterials.AMETHYST, 1, new Item.Properties().group(Reference.tabSlurpiesDongles));
@@ -53,6 +66,20 @@ public class ModTools {
     public static Item TOPAZ_HOE = new CustomHoe("topaz_hoe", ToolMaterials.TOPAZ, 1, new Item.Properties().group(Reference.tabSlurpiesDongles).rarity(Rarity.RARE));
     public static Item WITHERED_HOE = new CustomHoe("withered_hoe", ToolMaterials.WITHERED, 1, new Item.Properties().group(Reference.tabSlurpiesDongles).rarity(Rarity.EPIC));
 
+    //Lumber Axes
+    public static Item AMAZONITE_LUMBER_AXE = new LumbarAxe("amazonite_lumber_axe", ToolMaterials.AMAZONITE, 7, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item AMETHYST_LUMBER_AXE = new LumbarAxe("amethyst_lumber_axe", ToolMaterials.AMETHYST, 4, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item DIAMOND_LUMBER_AXE = new LumbarAxe("diamond_lumber_axe", ItemTier.DIAMOND, 3, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item EMERALD_LUMBER_AXE = new LumbarAxe("emerald_lumber_axe", ToolMaterials.EMERALD, 4, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item GOLD_LUMBER_AXE = new LumbarAxe("gold_lumber_axe", ItemTier.GOLD, 3, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item IRON_LUMBER_AXE = new LumbarAxe("iron_lumber_axe", ItemTier.IRON, 3, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item PERIDOT_LUMBER_AXE = new LumbarAxe("peridot_lumber_axe", ToolMaterials.PERIDOT, 3, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item RUBY_LUMBER_AXE = new LumbarAxe("ruby_lumber_axe", ToolMaterials.RUBY, 4, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item SAPPHIRE_LUMBER_AXE = new LumbarAxe("sapphire_lumber_axe", ToolMaterials.SAPPHIRE, 3, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item STONE_LUMBER_AXE = new LumbarAxe("stone_lumber_axe", ItemTier.STONE, 1, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item TOPAZ_LUMBER_AXE = new LumbarAxe("topaz_lumber_axe", ToolMaterials.TOPAZ, 7, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item WITHERED_LUMBER_AXE = new LumbarAxe("withered_lumber_axe", ToolMaterials.WITHERED, 8, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
+    public static Item WOODEN_LUMBER_AXE = new LumbarAxe("wooden_lumber_axe", ItemTier.WOOD, 1, -3.1F,  new Item.Properties().group(Reference.tabSlurpiesDongles));
     //Paxels
     public static Item AMAZONITE_PAXEL = new Paxel("amazonite_paxel", ToolMaterials.AMAZONITE_PAXEL, 4, -3.0F,  new Item.Properties().group(Reference.tabSlurpiesDongles), HarvestLevelHelper.AMAZONITE);
     public static Item AMETHYST_PAXEL = new Paxel("amethyst_paxel", ToolMaterials.AMETHYST_PAXEL, 4, -3.0F,  new Item.Properties().group(Reference.tabSlurpiesDongles), HarvestLevelHelper.AMETHYST);
---
+++ src/main/java/com/rbs/slurpiesdongles/items/tools/LumbarAxe.java
@@ -0,0 +1,123 @@
+package com.rbs.slurpiesdongles.items.tools;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import com.rbs.slurpiesdongles.Reference;
+import com.rbs.slurpiesdongles.helpers.ToolHelper;
+import com.rbs.slurpiesdongles.helpers.HarvestLevelHelper;
+import com.rbs.slurpiesdongles.init.ModTools;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+/*
+Source code for this class belongs too Neurodr0me, and can be found here: https://github.com/astradamus/PracticalTools
+Credit can also be found on the Contributors page of my wiki which is here: https://github.com/RedBullSlurpie/SlurpiesDongles/wiki/Contributors
+ */
+
+public class LumbarAxe extends AxeItem {
+
+    private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.OAK_PLANKS, Blocks.SPRUCE_PLANKS, Blocks.BIRCH_PLANKS, Blocks.JUNGLE_PLANKS, Blocks.ACACIA_PLANKS, Blocks.DARK_OAK_PLANKS, Blocks.BOOKSHELF, Blocks.OAK_WOOD, Blocks.SPRUCE_WOOD, Blocks.BIRCH_WOOD, Blocks.JUNGLE_WOOD, Blocks.ACACIA_WOOD, Blocks.DARK_OAK_WOOD, Blocks.OAK_LOG, Blocks.SPRUCE_LOG, Blocks.BIRCH_LOG, Blocks.JUNGLE_LOG, Blocks.ACACIA_LOG, Blocks.DARK_OAK_LOG, Blocks.CHEST, Blocks.PUMPKIN, Blocks.CARVED_PUMPKIN, Blocks.JACK_O_LANTERN, Blocks.MELON, Blocks.LADDER, Blocks.SCAFFOLDING, Blocks.OAK_BUTTON, Blocks.SPRUCE_BUTTON, Blocks.BIRCH_BUTTON, Blocks.JUNGLE_BUTTON, Blocks.DARK_OAK_BUTTON, Blocks.ACACIA_BUTTON, Blocks.OAK_PRESSURE_PLATE, Blocks.SPRUCE_PRESSURE_PLATE, Blocks.BIRCH_PRESSURE_PLATE, Blocks.JUNGLE_PRESSURE_PLATE, Blocks.DARK_OAK_PRESSURE_PLATE, Blocks.ACACIA_PRESSURE_PLATE);
+    public static final Set<Material> EFFECTIVE_MATERIALS = ImmutableSet.of(Material.WOOD, Material.GOURD, Material.CACTUS);
+    public static final int LOG_BREAK_DELAY = 1;
+
+
+    public LumbarAxe(String name, IItemTier tier, float attackDamageIn, float attackSpeedIn, Properties builder) {
+        super(tier, attackDamageIn, attackSpeedIn, builder);
+
+        this.setRegistryName(Reference.MODID, name);
+        ModTools.TOOLS.add(this);
+    }
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+        stack.attemptDamageItem(6, ToolHelper.random, null);//Set to 6 because generally a tree is 6 tall ish
+
+        if (entityLiving instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entityLiving;
+
+            if (!attemptFellTree(world, pos, player)) {
+                ToolHelper.attemptBreakNeighbors(world, pos, player, EFFECTIVE_ON, EFFECTIVE_MATERIALS);
+            }
+        }
+
+        return super.onBlockDestroyed(stack, world, state, pos, entityLiving);
+    }
+
+    private boolean attemptFellTree(World world, BlockPos pos, PlayerEntity player) {
+
+        ArrayList<BlockPos> logs = new ArrayList<>();
+        ArrayList<BlockPos> candidates = new ArrayList<>();
+        candidates.add(pos);
+
+        int leaves = 0;
+
+        for (int i = 0; i < candidates.size(); i++) {
+            if (logs.size() > 200) return false;
+
+            BlockPos candidate = candidates.get(i);
+            Block block = world.getBlockState(candidate).getBlock();
+
+            if (BlockTags.LEAVES.func_199685_a_(block)) {
+                leaves++;
+            }
+            else if (BlockTags.LOGS.func_199685_a_(block)) {
+                logs.add(candidate);
+
+
+                for (int x = -1; x <= 1; x++) {
+                    for (int y = 0; y <= 1; y++) {
+                        for (int z = -1; z <= 1; z++) {
+                            BlockPos neighbor = candidate.add(x, y, z);
+                            if (candidates.contains(neighbor)) continue;
+                            candidates.add(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (logs.size() == 0) return false;
+
+        if (leaves >= logs.size()/6.0) {
+            MinecraftForge.EVENT_BUS.register(new Object() {
+
+                int delay = LOG_BREAK_DELAY;
+                int i = 0;
+
+                @SubscribeEvent
+                public void onTick(TickEvent.WorldTickEvent event) {
+                    if (delay-- > 0) return;
+                    delay = LOG_BREAK_DELAY;
+                    if (i < logs.size()) {
+                        BlockPos log = logs.get(i);
+                        ToolHelper.attemptBreak(world, log, player, EFFECTIVE_ON, EFFECTIVE_MATERIALS);
+                        i++;
+                    }
+                    else {
+                        MinecraftForge.EVENT_BUS.unregister(this);
+                    }
+                }
+            });
+
+            return true;
+        }
+
+        return false;
+    }
+}
