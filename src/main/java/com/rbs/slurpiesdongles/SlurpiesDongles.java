cfc5c47e36f728c2af08931661b75d0505889f23
RedBullSlurpie <redbullslurpie@gmail.com>
2019-08-11 19:40:06 -0400

    Code clean up
    Code clean up and fix Update Checker for 1.14.4

--- src/main/java/com/rbs/slurpiesdongles/SlurpiesDongles.java
+++ src/main/java/com/rbs/slurpiesdongles/SlurpiesDongles.java
@@ -48,7 +48,6 @@ public class SlurpiesDongles {
         // some preinit code
         MinecraftForge.EVENT_BUS.register(new PigDrops());
         OreGenerator.setupOreGen();
-       // MinecraftForge.EVENT_BUS.register(new GrassDrops());
 
     }
 
